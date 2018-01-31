f5b29cd3abcf24f1ee8527097cc9943d1da0b3ba
uufish <uufish.io@gmail.com>
2018-02-01 00:01:59 +0700

    update: アップデートの追加

--- src/app/view-home/view-home.component.ts
+++ src/app/view-home/view-home.component.ts
@@ -12,11 +12,28 @@ export class ViewHomeComponent implements OnInit {
 
   public formGroup: FormGroup;
 
-  public submitForm() {
-    this.email.markAsDirty();
-    this.password.markAsDirty();
-    this.remember.markAsDirty();
-  }
+  public updates = [
+    {
+      content: 'ログイン機能を追加しました',
+      date: '2018-01-01'
+    },
+    {
+      content: 'ユーザ検索機能を追加しました',
+      date: '2018-02-01'
+    },
+    {
+      content: 'GitHub連携機能を追加しました',
+      date: '2018-03-01'
+    },
+    {
+      content: 'プロジェクト作成機能を追加しました',
+      date: '2018-04-01'
+    },
+    {
+      content: 'ユーザページ機能を追加しました',
+      date: '2018-05-01'
+    }
+  ];
 
   constructor(private fb: FormBuilder) {
   }
@@ -33,7 +50,13 @@ export class ViewHomeComponent implements OnInit {
     return this.formGroup.get('remember');
   }
 
-  ngOnInit() {
+  public submitForm() {
+    this.email.markAsDirty();
+    this.password.markAsDirty();
+    this.remember.markAsDirty();
+  }
+
+  public ngOnInit() {
     this.formGroup = this.fb.group({
       email: [
         null, [
