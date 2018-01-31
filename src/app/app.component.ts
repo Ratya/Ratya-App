f6f7eca2693e4b436348b82eca6a09a6def41dba
uufish <uufish.io@gmail.com>
2018-01-31 22:56:30 +0700

    update: sidebar logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  public logo = '/assets/logo-white.png';
+
   public nzTheme = 'dark';
 
   public nzMode = 'vertical';
