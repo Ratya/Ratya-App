a8b8942feafbec8d72433d4334bd1bbac7e7e295
uufish <uufish.io@gmail.com>
2018-02-01 00:09:59 +0700

    update: cards

--- src/app/view-updates/view-updates.component.ts
+++ src/app/view-updates/view-updates.component.ts
@@ -7,7 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewUpdatesComponent implements OnInit {
 
-  constructor() { }
+  public updates = [
+    {
+      content: 'ログイン機能を追加しました',
+      date: '2018-01-01',
+      title: 'ログイン機能を追加しました'
+    },
+    {
+      content: 'ログイン機能を追加しました',
+      date: '2018-02-01',
+      title: 'ユーザ検索機能を追加しました'
+    },
+    {
+      content: 'ログイン機能を追加しました',
+      date: '2018-03-01',
+      title: 'GitHub連携機能を追加しました'
+    },
+    {
+      content: 'ログイン機能を追加しました',
+      date: '2018-04-01',
+      title: 'プロジェクト作成機能を追加しました'
+    },
+    {
+      content: 'ログイン機能を追加しました',
+      date: '2018-05-01',
+      title: 'ユーザページ機能を追加しました'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
